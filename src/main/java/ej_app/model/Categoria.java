26da92e3734e6858fe03d311fb21f63cc6a12556
amendezkar <agusmendez2001@gmail.com>
2022-11-18 02:14:20 -0300

    Commit dummy

--- src/main/java/ej_app/model/Categoria.java
+++ src/main/java/ej_app/model/Categoria.java
@@ -1,9 +1,5 @@
 package ej_app.model;
 
-//Parte D: La personas tendrán que tener un campo autocalculado que se persista en la BD,
-//        que dependiendo la fecha de nacimiento categorice a las personas como:
-//        Niños < 11, Adolescentes >=11 & < 18, Adultos >=18 & < 80, Octogenario >= 80.
-
 public enum Categoria {
 
     NINIO,
